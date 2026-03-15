2e5b482061a32337229c2c6b3fee535d25ac5661
AllanRono1 <allanrono18@gmail.com>
2026-03-15 15:59:51 +0300

    Created a Main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,27 @@
+function Main() {
+    return (
+        <main className="location-info">
+            <div>
+                <div>
+                    <h1>Villa Elektra</h1>
+                    <p>Nairobi, KE</p>
+                    <span>View on google maps</span>
+                </div>
+                <div>
+                    <p>[1160 m<sup>2</sup>]</p>
+                    <p>[2 bedrooms]</p>
+                    <p>[2025]</p>
+                </div>
+                <div>
+                    <p>Experienced really heavy rainfalls. The lights were out for the most 
+                        part but there was no issue with the drainage system in the area</p>
+                </div>
+            </div>
+            <div>
+                <img src="/src/assets/images/square1.jpg" />
+            </div>
+        </main>
+    )
+}
+
+export default Main
