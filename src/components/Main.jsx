f0b3488625ffcbb20979cfaea8ed862cb36fd5d5
AllanRono1 <allanrono18@gmail.com>
2026-03-16 09:44:22 +0300

    Styled the main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,25 @@
 function Main() {
     return (
-        <main className="location-info">
+        <main className="location-container">
             <div>
-                <div>
+                <div className="location-info">
                     <h1>Villa Elektra</h1>
-                    <p>Nairobi, KE</p>
-                    <span>View on google maps</span>
+                    <p className="info-size">Nairobi, KE</p>
+                    <span className="info-size"><a href="#">View on google maps</a></span>
                 </div>
-                <div>
-                    <p>[1160 m<sup>2</sup>]</p>
+                <div className="location-specs">
+                    <p>[1160 m<sup className="mini-text">2</sup>]</p>
                     <p>[2 bedrooms]</p>
                     <p>[2025]</p>
                 </div>
-                <div>
+                <div className="location-description">
                     <p>Experienced really heavy rainfalls. The lights were out for the most 
                         part but there was no issue with the drainage system in the area</p>
                 </div>
             </div>
-            <div>
-                <img src="/src/assets/images/square1.jpg" />
-            </div>
+                <div className="image-container">
+                    <img src="/src/assets/images/square1.jpg" />
+                </div>
         </main>
     )
 }
