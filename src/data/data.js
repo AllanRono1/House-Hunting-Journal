589c4ae59a08ea87693cacd95c9f1123ee16e250
AllanRono1 <allanrono18@gmail.com>
2026-03-17 07:12:54 +0300

    Create: data.js file

---
+++ src/data/data.js
@@ -0,0 +1,76 @@
+const data = [
+    {
+        id: 1,
+        header: "Tunataka Pawa",
+        location: "Nairobi, KE",
+        locationLink: "https://maps.app.goo.gl/tLXx46nAgSPut29cA",
+        meters: "[1160 m",
+        availableRooms: "[4",
+        yearVisit: "[2024]",
+        comments: "Such a great experience 10/10",
+        src: "/src/assets/images/square1.jpg",
+        alt: "hot air balloon"
+    },
+
+    {
+        id: 1,
+        header: "Tunataka Pawa",
+        location: "Nairobi, KE",
+        locationLink: "https://maps.app.goo.gl/tLXx46nAgSPut29cA",
+        meters: "[1160 m",
+        availableRooms: "[4",
+        yearVisit: "[2024]",
+        comments: "Such a great experience 10/10",
+        src: "/src/assets/images/square1.jpg",
+        alt: "hot air balloon"
+    },
+
+    {
+        id: 1,
+        header: "Tunataka Pawa",
+        location: "Nairobi, KE",
+        locationLink: "https://maps.app.goo.gl/tLXx46nAgSPut29cA",
+        meters: "[1160 m",
+        availableRooms: "[4",
+        yearVisit: "[2024]",
+        comments: "Such a great experience 10/10",
+        src: "/src/assets/images/square1.jpg",
+        alt: "hot air balloon"
+    },
+
+    {
+        id: 1,
+        header: "Tunataka Pawa",
+        location: "Nairobi, KE",
+        locationLink: "https://maps.app.goo.gl/tLXx46nAgSPut29cA",
+        meters: "[1160 m",
+        availableRooms: "[4",
+        yearVisit: "[2024]",
+        comments: "Such a great experience 10/10",
+        src: "/src/assets/images/square1.jpg",
+        alt: "hot air balloon"
+    },
+
+    {
+        id: 1,
+        header: "Tunataka Pawa",
+        location: "Nairobi, KE",
+        locationLink: "https://maps.app.goo.gl/tLXx46nAgSPut29cA",
+        meters: "[1160 m",
+        availableRooms: "[4",
+        yearVisit: "[2024]",
+        comments: "Such a great experience 10/10",
+        src: "/src/assets/images/square1.jpg",
+        alt: "hot air balloon"
+    }
+]
+
+// header="Tunataka Pawa"
+//               location="Nairobi, KE"
+            //   locationLink="https://maps.app.goo.gl/tLXx46nAgSPut29cA"
+            //   meters="[1160 m"
+            //   availableRooms="[4"
+            //   yearVisit="[2024]"
+            //   comments="Such a great experience 10/10"
+            //   src="/src/assets/images/square1.jpg"
+            //   alt="hot air balloon"
